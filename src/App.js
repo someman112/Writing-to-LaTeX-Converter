e5018886d9d5fcd370447d8945f32bf47b658ca1
Abdullah Siddiqui <abdull.siddiqui@mail.utoronto.ca>
2024-02-17 18:49:12 -0500

    literally the whole app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import { useEffect, useRef, useState } from "react";
 import styles from './App.module.css';
 import Latex from "react-latex";
+import { fetchResponse } from "./api";
 
 function App() {
   const canvasRef = useRef();
   const contextRef = useRef();
 
   const [isDrawing, setIsDrawing] = useState(false);
+  const [latexContent, setLatexContent] = useState("");
+
+
 
   useEffect(() => {
     const canvas = canvasRef.current;
     canvas.width = 800;
-    canvas.height = 500;
-
+    canvas.height = 600;
     const context = canvas.getContext("2d");
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, canvas.width, canvas.height);
     context.lineCap = "round";
+    context.lineJoin = 'bevel';
     context.strokeStyle = "black";
-    context.lineWidth = 3;
+    context.lineWidth = 6;
     contextRef.current = context;
   }, []);
 
@@ -44,26 +50,45 @@ function App() {
     contextRef.current.stroke();
   };
 
+  const resetCanvas = () => {
+    // contextRef.current.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+    contextRef.fillStyle = 'white';
+    contextRef.current.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
+  };
+
+  const createRequest = async () => {
+    const data = canvasRef.current.toDataURL("image/png", 1.0);
+    const image_data = data.split(",")[1];
+    const response = await fetchResponse(image_data);
+    parseMessage(response);
+  }
+
+  const parseMessage = (response) => {
+    console.log(response);
+    setLatexContent(`$$${response}$$`);
+
+  }
+
   return (
-    <div className="App">
-      <h2>ScribbleTeX</h2>
-      <canvas
-        ref={canvasRef}
-        onMouseDown={beginDrawing}
-        onMouseMove={draw}
-        onMouseUp={stopDrawing}
-      />
-      <table>
-        <tr><input type="submit" name="process" value="process here" /*onclick={handler}*//></tr>
-        <tr><input type="submit" name="process" value="reset" /*onclick={handler}*//></tr>
-      </table>
-      <div className={styles.Latex}>
-        <Latex>What is $(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$$(3\times 4) \div (5-3)$</Latex>
+      <div className="App">
+        <h2>ScribbleTeX</h2>
+        <canvas
+            ref={canvasRef}
+            onMouseDown={beginDrawing}
+            onMouseMove={draw}
+            onMouseUp={stopDrawing}
+        />
+        <table>
+          <tr><input type="submit" name="process" value="process here" onClick={createRequest} /></tr>
+          <tr><input type="submit" name="process" value="reset" onClick={resetCanvas}/></tr>
+        </table>
+        <div className={styles.Latex}>
+          <Latex>{latexContent}</Latex>
+        </div>
+        <p>
+          p tag
+        </p>
       </div>
-      <p>
-        p tag
-      </p>
-    </div>
   );
 }
 
--- src/api.js
+++ src/api.js
@@ -1,3 +1,43 @@
-export const fetchResponse = async () => {
-    
+import OpenAI from "openai";
+
+const openai = new OpenAI({ apiKey: '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', dangerouslyAllowBrowser: true });
+
+export const fetchResponse = async (image_data) => {
+    const headers = {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${openai.apiKey}`
+    }
+
+    const payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": "Convert to a latex expression. Only output the latex, DON'T add any other words"
+                    },
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "data:image/jpeg;base64," + image_data
+                        }
+                    }
+                ]
+            }
+        ],
+        "max_tokens": 300
+    }
+
+    return fetch("https://api.openai.com/v1/chat/completions", {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(payload)
+    })
+        .then(response => response.json())
+        .then(data => data['choices'][0]['message']['content'])
+        .catch(error => console.error('Error:', error));
+
+
 }
